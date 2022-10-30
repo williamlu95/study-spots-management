20264bf0199f2a3ad6e784a5718997d7ef52df38
williamlu95 <3TFvHq9g4UQ89m9>
2022-10-30 16:14:15 -0700

    use lower case for import

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery } from '@mui/material';
 import Image from 'next/image';
-import LogoImage from '../public/Logo.png';
+import LogoImage from '../public/logo.png';
 
 export default function Logo(): JSX.Element {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
